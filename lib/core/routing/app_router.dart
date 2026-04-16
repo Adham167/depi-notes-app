23f6ae9374a00efaeb344b7448b75491799d124e
Adham167 <adhamabdelsalam3040@gmail.com>
2026-04-16 17:19:27 +0200

    initializing app and adding GoRouter package

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,14 @@
+import 'package:depi_notes_app/feature/auth/presentation/view/widgets/login_view.dart';
+import 'package:depi_notes_app/feature/auth/presentation/view/widgets/sign_up_view.dart';
+import 'package:go_router/go_router.dart';
+
+class AppRouter {
+  static const kSignUp = '/SignUp';
+
+  final GoRouter router = GoRouter(
+    routes: [
+      GoRoute(path: '/', builder: (context, state) => LoginView()),
+      GoRoute(path: kSignUp, builder: (context, state) => SignUpView()),
+    ],
+  );
+}
