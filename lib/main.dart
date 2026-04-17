7cd1bca8d3dae86f504d729a09a63a1bbd5c32cb
Adham167 <adhamabdelsalam3040@gmail.com>
2026-04-17 08:37:37 +0200

    initializing firebase in my project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:depi_notes_app/core/routing/app_router.dart';
+import 'package:depi_notes_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
